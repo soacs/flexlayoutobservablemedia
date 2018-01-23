fc5f84b7e7d22ce73f6d5ba53c59452289478bd2
LAPTOP-JHLSCDA7\steve <steve@soaconsultingservices.com>
2018-01-22 21:14:07 -0500

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,71 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import {Subscription} from 'rxjs/Subscription';
+import {MediaChange, ObservableMedia} from '@angular/flex-layout';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit, OnDestroy {
+  title = 'FlexLayoutModule Demo JavaScript API - ObservableMedia';
+  content: string;
+  invisible: boolean = false;
+  watcher: Subscription;
+  activeMediaQuery = '';
+
+  constructor(media: ObservableMedia) {
+    this.watcher = media.subscribe((change: MediaChange) => {
+      this.activeMediaQuery = change ? `'${change.mqAlias}' = (${change.mediaQuery})` : '';
+      if ( change.mqAlias === 'xs') {
+        this.loadMobileContent();
+      } else if ( change.mqAlias === 'lg') {
+        this.invisible = true;
+        this.loadDesktopContent();
+      } else if ( change.mqAlias === 'sm') {
+        this.loadSmallContent();
+      } else if ( change.mqAlias === 'md') {
+        this.loadMediumContent();
+      }
+    });
+  }
+
+  ngOnInit() {
+  }
+
+  isMobile() {
+    return false;
+  }
+
+  invisibleOnDesktop() {
+    return this.invisible;
+  }
+
+  ngOnDestroy() {
+    this.watcher.unsubscribe();
+  }
+
+  loadMobileContent() {
+    console.log('loading mobile content');
+    this.content = 'MOBILE CONTENT';
+  }
+
+  loadDesktopContent() {
+    console.log('loading desktop content');
+    this.content = 'DESKTOP CONTENT';
+    return this.invisible;
+  }
+
+  loadMediumContent() {
+    console.log('loading medium content');
+    this.content = 'MEDIUM CONTENT';
+  }
+
+  loadSmallContent() {
+    console.log('loading small content');
+    this.content = 'SMALL CONTENT';
+  }
+
+
 }
+
